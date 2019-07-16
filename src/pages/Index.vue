12a3184c6e294943b1c74fc41a32e948ccd938bd
Kessir Adjaho <m.kessir.adjaho@gmail.com>
2019-07-16 13:47:29 +0100

    Added tech skills

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,8 +5,11 @@
       
       <h2 class="text-5xl sm:text-6xl">Hi, I'm <span class="text-violet">Kessir</span> !</h2>
 
-      <p class="my-4 text-lg sm:text-2xl">Full-Stack Web Dev <span>&amp;</span> Android Developer</p>
+      <p class="text-4xl sm:text-2xl text-gray-700">Full-Stack Web Dev <span>&amp;</span> Mobile Developer</p>
 
+<p class="my-6 text-base max-w-2xl m-auto leading-relaxed">I work with technologies such as <span class="tech">Node.js</span> <span class="tech">SpringBoot</span> and <span class="tech">Vue.js</span> for web development.
+And <span class="tech">Kotlin</span> <span class="tech">Flutter</span> for mobile development.
+</p>
       <p class="my-6 text-violet text-sm sm:text-base">
         <a href="mailto:hi@kessir.com" target="_blank" rel="noopener">hi@kessir.com</a> | 
         <a href="https://github.com/kessir" target="_blank" rel="noopener">github.com/kessir</a>
