055b13c1e508de909b5b89eb9c5a7a02933a160a
Kessir Adjaho <m.kessir.adjaho@gmail.com>
2019-08-16 17:01:51 +0300

    Now mentions afrodevpodcast.com

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,49 @@
 <template>
   <Layout>
     <div class="text-center">
-      <img class="h-48 m-auto" src="../../static/dev-illustration.svg" alt="">
-      
-      <h2 class="text-5xl sm:text-6xl">Hi, I'm <span class="text-violet">Kessir</span> !</h2>
+      <img class="h-48 m-auto" src="../../static/dev-illustration.svg" alt />
 
-      <p class="text-xl sm:text-2xl text-gray-700">Full-Stack Web Dev <span>&amp;</span> Mobile Developer</p>
+      <h2 class="text-5xl sm:text-6xl">
+        Hi, I'm
+        <span class="text-violet">Kessir</span> !
+      </h2>
 
-<p class="my-6 text-base max-w-3xl m-auto leading-loose">I work with technologies such as <span class="tech">Node.js</span> <span class="tech">SpringBoot</span> and <span class="tech">Vue.js</span> for web development. <br>
-And <span class="tech">Kotlin</span> <span class="tech">Flutter</span> for mobile development.
-</p>
-      <p class="my-6 text-violet text-sm sm:text-base">
-        <a href="mailto:hi@kessir.com" target="_blank" rel="noopener">hi@kessir.com</a> | 
+      <p class="text-xl sm:text-2xl text-gray-700">
+        Full-Stack Web Dev
+        <span>&amp;</span> Mobile Developer
+      </p>
+
+      <p class="mt-6 text-base max-w-3xl m-auto leading-loose">
+        I work with technologies such as
+        <span class="tech">Node.js</span>
+        <span class="tech">SpringBoot</span> and
+        <span class="tech">Vue.js</span> for web development.
+        <br />And
+        <span class="tech">Kotlin</span>
+        <span class="tech">Flutter</span> for mobile development.
+      </p>
+      <p class="mt-8 text-sm max-w-lg m-auto text-gray-700">
+        In my spare time I host
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-indigo-700 underline uppercase text-xs"
+          href="https://afrodevpodcast.com/"
+        >AfrodevPodcast</a>
+, a podcast where I have interesting conversations with other african software developers.
+      </p>
+      <p class="mt-8 mb-6 text-violet text-sm sm:text-base">
+        <a href="mailto:hi@kessir.com" target="_blank" rel="noopener">hi@kessir.com</a> |
         <a href="https://github.com/kessir" target="_blank" rel="noopener">github.com/kessir</a>
       </p>
     </div>
   </Layout>
-  </template>
+</template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Software Engineer'
+    title: "Software Engineer"
   }
 };
 </script>
